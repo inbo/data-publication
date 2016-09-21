32af6c13d9ae292569fe27555fe0e8e08745e217
stijnvanhoey <stijnvanhoey@gmail.com>
2016-09-21 18:01:44 +0200

    adapt query to incorporate vernacularname

--- datasets/florabank1-occurrences/mapping/dwc-occurrence.sql
+++ datasets/florabank1-occurrences/mapping/dwc-occurrence.sql
@@ -1,12 +1,13 @@
 USE [Flora_IPT]
 GO
 
-/****** Object:  View [ipt].[vwGBif]    Script Date: 03/03/2016 16:57:25 ******/
+/****** Object:  View [ipt].[vwGBif]    Script Date: 21/09/2016 12:54:38 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 ALTER VIEW [ipt].[vwGBif] 
@@ -54,7 +55,7 @@ SELECT
 	    , [license] = N'http://creativecommons.org/publicdomain/zero/1.0/'
 		, [rightsHolder] = 'INBO'
         , [accessRights] = N'http://www.inbo.be/en/norms-for-data-use'
-		, [bibliographicCitation] = CONVERT(Nvarchar(300),'Van Landuyt W, Vanhecke L, Brosens D (2012) Florabank 1: a grid-based database on vascular plant distribution in the northern part of Belgium (Flanders and the Brussels Capital region). PhytoKeys 12: 59–67. doi: team.3897/phytokeys.12.2849' )
+		, [bibliographicCitation] = CONVERT(Nvarchar(300),'Van Landuyt W, Vanhecke L, Brosens D (2012) Florabank 1: a grid-based database on vascular plant distribution in the northern part of Belgium (Flanders and the Brussels Capital region). PhytoKeys 12: 59�67. doi: team.3897/phytokeys.12.2849' )
 		, [references] = cB.BRON_DES
 		, [datasetID] = CONVERT(Nvarchar(100),'http://dx.doi.org/10.3897/phytokeys.12.2849')
 		, [institutionCode] = CONVERT(Nvarchar(20),'INBO')
@@ -74,16 +75,17 @@ SELECT
 		, [countryCode] = CONVERt(Nvarchar(20),'BE')  
 		, [verbatimLocality] = tW.WRNG_OPM 
 		, [verbatimCoordinates] = tIH.IFBL_CDE
-	    , [verbatimCoordinateSystem] = CASE 
-										WHEN LEN([tIH].[IFBL_CDE]) > 5 THEN 'IFBL 1km'
-										ELSE 'IFBL 4km'
+	    , [verbatimCoordinateSystem] = CASE
+										WHEN LEN([tIH].[IFBL_CDE]) > 5 THEN 'IFBL 1x1km'
+										ELSE 'IFBL 4x4km'
 									   END
+		, [verbatimSRS] = 'Belgian Datum 1972'
 		, [decimalLatitude] = CONVERT(Nvarchar(20),convert(decimal(12,5),round(tI84.IFBL_LAT,5)) )
 		, [decimalLongitude] = CONVERT(Nvarchar(20),convert(decimal(12,5),round(tI84.IFBL_LONG,5)) ) 
 		, [geodeticDatum] = CONVERT(Nvarchar(10),'WGS84')
 		, [coordinateUncertaintyInMeters] = CONVERT(Nvarchar(5),
 			CONVERT(INT, CASE 
-				WHEN cHP.HOPR_CDE = 1 THEN 	700  --zeker kmhok
+				WHEN cHP.HOPR_CDE = 1 THEN 	707  --zeker kmhok
 				WHEN cHP.HOPR_CDE = 2 THEN 	2800  --zeker uurhok
 				WHEN cHP.HOPR_CDE = 3 THEN 	10000  --onzeker uurhok
 				WHEN cHP.HOPR_CDE = 4 THEN 	1000  --voorkeur kmhok
@@ -91,19 +93,21 @@ SELECT
 				ELSE 10000	
 			END )
 			)
-		, [georeferenceRemarks] = CONVERT(Nvarchar(100),'The centroid coördinates of the IFBL square containing the occurence were given')
+		, [georeferenceRemarks] = CONVERT(Nvarchar(100),'coordinates are centroid of 1x1km IFBL square')
 		
 		/* Category: Taxon */
 		, [scientificName] = ns.RECOMMENDED_SCIENTIFIC_NAME
 		, [kingdom] = CONVERT(Nvarchar(10),'Plantae')
 		, [taxonRank] = NS.RECOMMENDED_NAME_RANK_LONG
 		, [scientificNameAuthorship] = NS.RECOMMENDED_NAME_AUTHORITY + ISNULL ( ' ' + NS.RECOMMENDED_NAME_QUALIFIER , '')
+		, [vernacularName] = NormNaam.ITEM_NAME
 		, [nomenclaturalCode] = CONVERT(Nvarchar(20),'ICBN')
 
-		/* Following fields are mentioned in the data paper doi: 10.3897/phytokeys.12.2849, butnot taken into account in current updates */
+		/* Following fields are mentioned in the data paper doi: 10.3897/phytokeys.12.2849, butnot taken into account in current updates:
 		-- originalNameUsage
 		-- verbatimTaxonrank
 		-- modified
+		*/
 		
 	FROM [dbo].[tblWaarneming] tW 
 		INNER JOIN [dbo].[tblMeting] tMt ON ( tMt.METI_WRNG_ID = tW.WRNG_ID )
@@ -111,32 +115,30 @@ SELECT
 		INNER JOIN [dbo].[tblIFBLWGS84] tI84 ON ( tI84.IFBL_ID = tIH.IFBL_ID  ) 
 		LEFT JOIN  [dbo].[cdeHokPrecisie] cHP ON ( tW.WRNG_HOPR_CDE = cHP.HOPR_CDE )
 		INNER JOIN [dbo].[cdeWaarnemingStatus] cWS ON ( tW.WRNG_WGST_CDE = cWS.WGST_CDE )
-		INNER JOIN [dbo].[cdeBron] cB ON ( cB.BRON_CDE = tW.WRNG_BRON_CDE )
-		--LEFT JOIN [dbo].[relWaarnemingMedewerker] rWM ON ( tW.WRNG_ID = rWM.WGMW_WRNG_ID )
-		--LEFT JOIN [dbo].[cdeWaarnemingMedewerkerType] cWMT ON ( cWMT.WMTP_CDE = rWM.WGMW_WMTP_CDE )
-		--LEFT JOIN [dbo].[tblMedewerker] tM ON ( rWM.WGMW_MDWK_ID = tM.MDWK_ID )
-		--LEFT JOIN [dbo].[Person] P ON (tM.MDWK_PERS_CDE = P.PERS_CDE)
-		
+		INNER JOIN [dbo].[cdeBron] cB ON ( cB.BRON_CDE = tW.WRNG_BRON_CDE )		
 		INNER JOIN [dbo].[tblTaxon] tT ON (tT.TAXN_ID = tMt.METI_TAXN_ID)
-		INNER JOIN [NBNData].[inbo].[NameServer_12] NS ON ( NS.INBO_TAXON_VERSION_KEY = tT.TAXN_VRS_KEY COLLATE Latin1_General_CI_AI )
+		INNER JOIN [NBNData].[inbo].[NameServer_12] NS ON ( NS.INBO_TAXON_VERSION_KEY = tT.TAXN_VRS_KEY  COLLATE Latin1_General_CI_AI )
+		INNER JOIN [NBNData].[dbo].TAXON_VERSION TTV on TTV.TAXON_VERSION_KEY = NS.INBO_TAXON_VERSION_KEY_FOR_RECOMMENDED_NAME
+		INNER join [NBNData].[dbo].TAXON TA ON TA.TAXON_KEY = TTV.TAXON_KEY
+
+		--Normalizing to Vernacular names
+		LEFT OUTER JOIN (SELECT T.* 
+							FROM [NBNData].[dbo].INDEX_TAXON_NAME ITN
+								INNER JOIN [NBNData].[dbo].TAXON_LIST_ITEM TLI ON TLI.TAXON_LIST_ITEM_KEY = ITN.TAXON_LIST_ITEM_KEY
+								INNER join [NBNData].[dbo].TAXON_VERSION TV on TV.TAXON_VERSION_KEY = TLI.TAXON_VERSION_KEY
+								INNER join [NBNData].[dbo].TAXON T ON T.TAXON_KEY = TV.TAXON_KEY
+							WHERE T.LANGUAGE ='nl'
+							) NormNaam on NormNaam.TAXON_KEY = TA.TAXON_KEY
 		INNER JOIN [dbo].[cdeMetingStatus] cMS ON (cms.MEST_CDE = tMt.METI_MEST_CDE)
 		INNER JOIN [dbo].[cdeWaarnemingStatus] cW ON (cW.WGST_CDE = tW.WRNG_WGST_CDE)
-	--dbo.cdeWaarnemingMedewerkerType
 	WHERE 1=1 
-	--AND tW.WRNG_ID = 20
-	--AND tMt.METI_USR_CRE_DTE IS NOT NULL
 	AND  (tW.WRNG_BEG_DTE >= CONVERT(Date , '1972-01-01' , 120)
 		OR (tW.WRNG_BEG_DTE < CONVERT(Date , '1972-01-01' , 120) 
-			AND cB.BRON_DES not like '%streep%') )
+		AND cB.BRON_DES not like '%streep%') )
 	AND cMS.MEST_CDE in ( 'GDGA','GDGK' )
 	AND cW.WGST_CDE = 'GCTR'
 	AND NS.[INFORMAL GROUP] IN ('biesvaren', 'wolfsklauw', 'varen', 'paardenstaart', 'ginkgo', 'conifeer', 'bloemplant')
 
-	--ORDER BY tW.WRNG_ID, tMt.METI_ID
-		
-
-
 GO
-
 
 
