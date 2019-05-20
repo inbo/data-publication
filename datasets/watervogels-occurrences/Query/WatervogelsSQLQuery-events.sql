be02529af83975c203e5de7473cdc3d991903414
DimEvil <dimibrosens@gmail.comm>
2019-05-20 15:39:51 +0200

    Update WatervogelsSQLQuery-events.sql
    update query

--- datasets/watervogels-occurrences/Query/WatervogelsSQLQuery-events.sql
+++ datasets/watervogels-occurrences/Query/WatervogelsSQLQuery-events.sql
@@ -1,33 +1,53 @@
 USE [W0004_00_Waterbirds]
 GO
 
-/****** Object:  View [iptdev].[vwGBIF_INBO_Watervogels_new_events]    Script Date: 13/05/2019 12:35:32 ******/
+/****** Object:  View [iptdev].[vwGBIF_INBO_Watervogels_events]    Script Date: 20/05/2019 14:35:32 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-/*** 2019-05-13 queryopnieuw gedaan***/
 
-/**ALTER VIEW [iptdev].[vwGBIF_INBO_Watervogels_events]
-AS**/
+
+
+
+
+ALTER VIEW [iptdev].[vwGBIF_INBO_Watervogels_events]
+AS
 
 
 SELECT  
-      [eventID ] = N'INBO:WATERVOGELS:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) ,dsa.SampleKey),10)  
-	, [type] = N'Event'
-	, [language] = N'en'
+
+  ---RECORD ---
+
+      [type] = N'Event'
+   	, [language] = N'en'
 	, [license] = N'http://creativecommons.org/publicdomain/zero/1.0/'
 	, [rightsHolder] = N'INBO'
 	, [accessRights] = N'https://github.com/LifeWatchINBO/norms-for-data-use'
 	, [datasetID] = N'http://doi.org/10.15468/lj0udq'
-	, [institutionCode] = N'INBO'
 	, [datasetName] = N'Watervogels - Wintering waterbirds in Flanders, Belgium'
+	, [institutionCode] = N'INBO'
 	, [ownerInstitutionCode] = N'INBO'
+	
+	
+  ---EVENT---	
+	
+	, [eventID ] = N'INBO:WATERVOGELS:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) ,dsa.SampleKey),10)  
+	, [parentEventID] = N'INBO:WATERVOGELS:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) ,di.SurveyKey),10)
 	, [basisOfRecord] = N'HumanObservation'
-	, dsa.IceCoverDescription
-	, [eventRemarks] = 
+	, [samplingProtocol] = CASE SurveyCode
+							WHEN 'ZSCH' THEN 'Survey from boat'
+							WHEN 'NOORD' THEN 'Survey from boat'
+							ELSE 'Survey from land'
+							END
+	, [samplingEffort] = case CoverageDescription
+							WHEN 'Volledig' THEN 'all waterbirds counted'
+							WHEN 'Onvolledig' THEN 'not all waterbirds counted'
+							END
+	,[eventDate] = SampleDate
+	,[eventRemarks] = 
 		'{'
 		+
 		CASE samplecondition
@@ -65,22 +85,37 @@ SELECT
 		END
 		+
 		'}'
-		, SampleDate as 'eventDate'
-		, SurveyNaam
-		, surveyCode
-		, [sampleProtocol] = CASE SurveyCode 
-			WHEN 'ZSCH' then 'Count On Boat'
-			ELSE 'Count on Land'
-			End
-		, LocationWVNaam
-		, LocationWVCode
-		, sa.LocationWVKey
-		, LocationGeometry
+	
+	---LOCATION
+	, [locationID] = N'INBO:WATERVOGELS:LOCATION:' + Right( N'000000000' + CONVERT(nvarchar(20) ,DiL.LocationWVCode),10) 
+	, [continent] = N'Europe'
+	, [waterbody] = Dil.LocationWVNaam
+	, [countryCode] = N'BE'
+	, [varbatimLocality] = DiL.LocationWVNaam
+	, [locationRemarks] = N'cite centroid' 
+	, CONVERT(decimal(10,5), Dil.LocationGeometry.STCentroid().STY) as decimalLatitude
+	, CONVERT(decimal(10,5), Dil.LocationGeometry.STCentroid().STX) as decimalLongitude
+	, [geodeticDatum] = N'WGS84'
+
+	, Dil.LocationGeometry as LocationGeometry
+	, Dil.LocationGeometry.STCentroid() as LocationGeometryCentroid
+	, Dil.LocationGeometry.STCentroid().STAsText() as LocationGeometryCentroidTekst
 
 
+	, SurveyCode
+	, SurveyNaam
+	--- , dsa.IceCoverDescription
 
 FROM dbo.DimSample dsa 
  INNER JOIN (SELECT DISTINCT(SampleKey), SurveyKey, EventKey, LocationWVKey, SampleDateKey FROM FactTaxonOccurrence Fta WHERE SampleKey > 0) as Sa ON sa.sampleKey = dsa.SampleKey
- INNER JOIN dbo.DimSurvey Di on DI.SurveyKey = SA.SurveyKey
+ INNER JOIN dbo.DimSurvey Di on Di.SurveyKey = Sa.SurveyKey
+							AND Di.SurveyCode IN ('ZSCH','NOORD','MIDMA')
  INNER JOIN dbo.DimLocationWV DiL on DiL.LocationWVKey = Sa.LocationWVKey
- GO
+									
+--SELECT  *FROM DimSample
+--SELECT  *FROM DimSurvey
+
+
+GO
+
+
