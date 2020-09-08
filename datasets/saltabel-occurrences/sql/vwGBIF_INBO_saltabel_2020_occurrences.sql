3a239e699f18c01f7adeff943238710705da50de
DimEvil <dimibrosens@gmail.comm>
2020-09-08 14:45:31 +0200

    Update vwGBIF_INBO_saltabel_2020_occurrences.sql
    ready for whip
    https://github.com/inbo/data-publication/issues/57

--- datasets/saltabel-occurrences/sql/vwGBIF_INBO_saltabel_2020_occurrences.sql
+++ datasets/saltabel-occurrences/sql/vwGBIF_INBO_saltabel_2020_occurrences.sql
@@ -1,7 +1,7 @@
 USE [D0017_00_NBNData]
 GO
 
-/****** Object:  View [ipt].[vwGBIF_Saltabel_2020]    Script Date: 4/09/2020 9:10:50 ******/
+/****** Object:  View [ipt].[vwGBIF_Saltabel_2020]    Script Date: 8/09/2020 10:26:28 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,12 +11,8 @@ GO
 
 
 
-
-
-
-
-/**ALTER VIEW [ipt].[vwGBIF_Saltabel_2020]
-AS**/
+ALTER VIEW [ipt].[vwGBIF_Saltabel_2020]
+AS
 
 
 /*********************************************************/
@@ -56,30 +52,30 @@ SELECT
 			
 
 		
-		, [Type] = CASE WHEN RT.SHORT_NAME  IN ('auditory record', 'reference/auditory record' ) THEN 'Sound'
+		, [type] = CASE WHEN RT.SHORT_NAME  IN ('auditory record', 'reference/auditory record' ) THEN 'Sound'
 						WHEN RT.SHORT_NAME  IN ( 'field record/photographed', '' ) THEN 'StillImage'
-						WHEN RT.SHORT_NAME  IN ( 'Collection/auditory record', 'Collection', 'Collection/field record', 'Collection/reference') THEN 'PhysicalObject'
+						WHEN RT.SHORT_NAME  IN ( 'Collection/auditory record', 'Collection', 'Collection/field record', 'Collection/reference') OR TAOC.[Collection] is not NULL THEN 'PhysicalObject'
 						WHEN RT.SHORT_NAME  IN ( 'Reference' ) THEN 'Text'
 						WHEN RT.SHORT_NAME  IN ( 'field record', 'None', 'reported to recorder', 'trapped in Malaise trap' ) THEN 'Event'
 						ELSE 'Unknown' END 
-        , [Language] = CONVERT(Nvarchar(20),'en')
+        , [language] = CONVERT(Nvarchar(20),'en')
 		, [license] = N'http://creativecommons.org/publicdomain/zero/1.0/'
-		, [RightsHolder] =  CONVERT(Nvarchar(20),'INBO')
+		, [rightsHolder] =  CONVERT(Nvarchar(20),'INBO')
 		, [accessRights] = N'http://www.inbo.be/en/norms-for-data-use'
-	    , [DatasetID] = CONVERT(Nvarchar(100),'https://doi.org/10.15468/1rcpsq')
-	    , [DatasetName] = CONVERT(Nvarchar(200),S.ITEM_NAME) + ' - Orthoptera in Belgium' 
-		, [InstitutionCode] = CONVERT(Nvarchar(20),'INBO') 
+	    , [datasetID] = CONVERT(Nvarchar(100),'https://doi.org/10.15468/1rcpsq')
+	    , [datasetName] = CONVERT(Nvarchar(200),S.ITEM_NAME) + ' - Orthoptera in Belgium' 
+		, [institutionCode] = CONVERT(Nvarchar(20),'INBO') 
 		-- , [dataGeneralizations] = CONVERT(Nvarchar(100),'Coordinate Uncertainty In Meters depends on original UTM1 or UTM5 squares.')
 
 		
 
 		---EVENT---	
 
-		, [BasisOfRecord] = CASE  WHEN RT.SHORT_NAME  IN ('Collection', 'Collection/auditory record', 'Collection/field record', 'Collection/reference' ) THEN 'PreservedSpecimen'
+		, [basisOfRecord] = CASE  WHEN RT.SHORT_NAME  IN ('Collection', 'Collection/auditory record', 'Collection/field record', 'Collection/reference' ) THEN 'PreservedSpecimen'
 						  WHEN RT.SHORT_NAME  IN ('field record', 'field record/photographed', 'None' ) THEN 'HumanObservation'
 						  ELSE 'HumanObservation' 
 						  END
-		, [EventDate] = CASE  [inbo].[LCReturnVagueDateGBIF]( SA.VAGUE_DATE_START, SA.VAGUE_DATE_END , SA.VAGUE_DATE_TYPE, 0)  --1 changed to 0 for dateranges
+		, [eventDate] = CASE  [inbo].[LCReturnVagueDateGBIF]( SA.VAGUE_DATE_START, SA.VAGUE_DATE_END , SA.VAGUE_DATE_TYPE, 0)  --1 changed to 0 for dateranges
 								WHEN 'unknown' then NULL
 								ELSE [inbo].[LCReturnVagueDateGBIF]( SA.VAGUE_DATE_START, SA.VAGUE_DATE_END , SA.VAGUE_DATE_TYPE, 0)
 								END
@@ -91,47 +87,67 @@ SELECT
 		
 		, [continent] = N'Europe'
 	    , [countryCode] = N'BE'
-		, [Municipality] = CONVERT (Nvarchar(500), LN.ITEM_NAME)
+		, [municipality] = CONVERT (Nvarchar(500), LN.ITEM_NAME)
 		, [decimalLatitude]  =CONVERT(Nvarchar(20),convert(decimal(12,5),round(Coalesce(SA.Lat ,0),5)) ) 
 		, [decimalLongitude] = CONVERT(Nvarchar(20),convert(decimal(12,5),round(Coalesce(SA.Long,0),5)) )
 		, [geodeticDatum] = CONVERT(Nvarchar(10),'WGS84') 
 	--	, [verbatimSRS] = N'BD72'
 		, [verbatimcoordinates] = CASE SA.SPATIAL_REF_QUALIFIER  
-							WHEN 'Centrd UTM5 x dgmnte' THEN sa.SPATIAL_REF
-							WHEN 'Centroïd deelgemnte' THEN sa.SPATIAL_REF
-							WHEN 'Centroïd UTM 1 km' THEN SDA.DATA
-							WHEN 'Centroïd UTM 5 km' THEN SDA.DATA
-							WHEN 'Imported' THEN sa.SPATIAL_REF
-							WHEN 'XY from original rec' THEN sa.SPATIAL_REF
+							WHEN 'Centrd UTM5 x dgmnte' THEN NULL                --sa.SPATIAL_REF
+							WHEN 'Centroïd deelgemnte'  THEN NULL --sa.SPATIAL_REF  --  THEN CONVERT(Nvarchar(20),ROUND(sa.SPATIAL_REF,2))
+							WHEN 'Centroïd UTM 1 km' THEN '31U' + SDA.DATA
+							WHEN 'Centroïd UTM 5 km' THEN '31U' + SDA.DATA
+							WHEN 'Imported' THEN NULL -- sa.SPATIAL_REF
+							WHEN 'XY from original rec' THEN NULL --sa.SPATIAL_REF
 							ELSE SA.SPATIAL_REF
 							END
+		--, [lambert] = sa.SPATIAL_REF
+		, [verbatimLongitude] = CASE  SA.SPATIAL_REF_QUALIFIER 
+							WHEN 'Centrd UTM5 x dgmnte' THEN ROUND (substring(sa.spatial_REF, 1,charindex(',',sa.SPATIAL_REF)-1),2)                --sa.SPATIAL_REF
+							WHEN 'Centroïd deelgemnte'  THEN ROUND (substring(sa.spatial_REF, 1,charindex(',',sa.SPATIAL_REF)-1),2)                --sa.SPATIAL_REF  --  THEN CONVERT(Nvarchar(20),ROUND(sa.SPATIAL_REF,2))
+							WHEN 'Centroïd UTM 1 km'    THEN NULL  --SDA.DATA
+							WHEN 'Centroïd UTM 5 km'    THEN NULL  --SDA.DATA
+							WHEN 'Imported'             THEN ROUND (substring(sa.spatial_REF, 1,charindex(',',sa.SPATIAL_REF)-1),2) -- sa.SPATIAL_REF
+							WHEN 'XY from original rec' THEN ROUND (substring(sa.spatial_REF, 1,charindex(',',sa.SPATIAL_REF)-1),2) --sa.SPATIAL_REF
+							ELSE SA.SPATIAL_REF
+							END					
+		, [verbatimLatitude] = CASE  SA.SPATIAL_REF_QUALIFIER 
+							WHEN 'Centrd UTM5 x dgmnte' THEN ROUND ((substring(sa.spatial_REF, charindex(',',sa.SPATIAL_REF) +1 ,11)),2)                --sa.SPATIAL_REF
+							WHEN 'Centroïd deelgemnte'  THEN ROUND ((substring(sa.spatial_REF, charindex(',',sa.SPATIAL_REF) +1 ,11)),2)               --sa.SPATIAL_REF  --  THEN CONVERT(Nvarchar(20),ROUND(sa.SPATIAL_REF,2))
+							WHEN 'Centroïd UTM 1 km'    THEN NULL  --SDA.DATA
+							WHEN 'Centroïd UTM 5 km'    THEN NULL  --SDA.DATA
+							WHEN 'Imported'             THEN ROUND ((substring(sa.spatial_REF, charindex(',',sa.SPATIAL_REF) +1 ,11)),2) -- sa.SPATIAL_REF
+							WHEN 'XY from original rec' THEN ROUND ((substring(sa.spatial_REF, charindex(',',sa.SPATIAL_REF) +1 ,11)),2) --sa.SPATIAL_REF
+							ELSE SA.SPATIAL_REF
+							END		
+		
+		
+		 
 		, [verbatimcoordinateSystem] = CASE SA.SPATIAL_REF_QUALIFIER  
 							WHEN 'Centrd UTM5 x dgmnte' THEN 'Lambert coordinates'
-							WHEN 'Centroïd deelgemnte' THEN sa.SPATIAL_REF
+							WHEN 'Centroïd deelgemnte' THEN 'Lambert coordinates'
 							WHEN 'Centroïd UTM 1 km' THEN 'UTM 1km'
 							WHEN 'Centroïd UTM 5 km' THEN 'UTM 5km'
 							WHEN 'Imported' THEN 'Lambert coordinates'
 							WHEN 'XY from original rec' THEN 'Lambert coordinates'
-							ELSE SA.SPATIAL_REF
+							ELSE SA.SPATIAL_REF_QUALIFIER
 							END
 		, [verbatimSRS] = CASE SA.SPATIAL_REF_QUALIFIER  
-							WHEN 'Centrd UTM5 x dgmnte' THEN 'BD72'
+							WHEN 'Centrd UTM5 x dgmnte' THEN 'Belgian Date 1972'
 							WHEN 'Centroïd deelgemnte' THEN sa.SPATIAL_REF
 							WHEN 'Centroïd UTM 1 km' THEN 'WGS84'
 							WHEN 'Centroïd UTM 5 km' THEN 'WGS84'
-							WHEN 'Imported' THEN 'BD72'
-							WHEN 'XY from original rec' THEN 'BD72'
+							WHEN 'Imported' THEN 'Belgian Datum 1972'
+							WHEN 'XY from original rec' THEN 'Belgian Datum 1972'
 							ELSE SA.SPATIAL_REF
 							END
 		
 		
 		--	, [REFgrid] = SA.SPATIAL_REF_QUALIFIER
-	    , [lambert] = sa.SPATIAL_REF
-		, [verbatimLongitude] = substring(sa.spatial_REF, 1,charindex(',',sa.SPATIAL_REF)-1)
-		, [verbatimLatitude] = (substring(sa.spatial_REF, charindex(',',sa.SPATIAL_REF) +1 ,11))
+	    
 	--	, sa.SPATIAL_REF
 	
-		, [CoordinateUncertaintyInMeters] = convert(nvarchar(5),coalesce(case
+		, [coordinateUncertaintyInMeters] = convert(nvarchar(5),coalesce(case
 																	WHEN SA.SPATIAL_REF_QUALIFIER = 'Centrd UTM5 x dgmnte' THEN '3536'
 																	WHEN SA.SPATIAL_REF_QUALIFIER='Centroïd deelgemnte' THEN '3536'
 																	WHEN SA.SPATIAL_REF_QUALIFIER='Centroïd UTM 1 km' THEN '707'
@@ -158,12 +174,12 @@ SELECT
   --                                                                  else null
   --                                                                  end,'70')) 
 		, [georeferenceRemarks] = CASE SA.SPATIAL_REF_QUALIFIER  
-							WHEN 'Centrd UTM5 x dgmnte' THEN 'UTM5 km centroid x municipality intersect'
-							WHEN 'Centroïd deelgemnte' THEN 'centroid municipality'
-							WHEN 'Centroïd UTM 1 km' THEN 'UTM1 km centroid'
-							WHEN 'Centroïd UTM 5 km' THEN 'UTM5 km centroid'
-							WHEN 'Imported' THEN 'imported coordinate'
-							WHEN 'XY from original rec' THEN 'point coordinate'
+							WHEN 'Centrd UTM5 x dgmnte' THEN 'coordinates are centroid of 5km grid square and municipality intersection'
+							WHEN 'Centroïd deelgemnte' THEN 'coordinates are centroid of municipality'
+							WHEN 'Centroïd UTM 1 km' THEN 'coordinates are centroid of 1km grid square'
+							WHEN 'Centroïd UTM 5 km' THEN 'coordinates are centroid of 5km grid square'
+							WHEN 'Imported' THEN 'coordinates are centroid of 100m grid square'
+							WHEN 'XY from original rec' THEN 'coordinates are original point coordinates'
 							ELSE SA.SPATIAL_REF_QUALIFIER
 							END
 
@@ -189,39 +205,62 @@ SELECT
 									END
 		--, [organismQuantity] = taoMeas.DATA
 		--, [abutest2] =  SUM (CASE WHEN ISNUMERIC(taoMeas.DATA) = 1 AND taoMeas.ACCURACY = 'exact' THEN CONVERT(int , taoMeas.DATA) ELSE NULL END ) 
-		, LifestageMeas.lifeStage
-		, lifeStage5
-	--	, [lifeStage_ind]
-	    --	, SexMeas.sex
-		, [occurrenceRemarks] = COALESCE(SexMeas.sex, '') + 
-			Case when COALESCE(SexMeas.sex, '') <> '' AND COALESCE(LifestageMeas.lifeStage, '') <> ''  THEN '|' ELSE '' END +
-			COALESCE ( LifestageMeas.lifeStage5, '' ) 
-		, LifestageMeas.rauw as LifestagRauw
-		-- , SexMeas.rauw as SexRauw
-		, [behaviour] = CASE behaviour
+		, [lifeStage] = CASE LifestageMeas.lifeStage
+							WHEN 'adult' THEN 'adult'
+							WHEN 'adult | juvenile' THEN 'adult | juvenile'
+							WHEN 'juvenile' THEN 'juvenile'
+							WHEN 'juvenile | adult' THEN 'adult | juvenile'
+							ELSE NULL
+							END
+		, [behavior] = CASE behaviour
 		                  WHEN 'adult | Vleugel | Zangpost' THEN 'stridulating'
 						  WHEN 'adult | Zangpost' THEN 'stridulating'
 						  WHEN 'Zangpost' THEN 'stridulating'
 						  ELSE NULL
 						  END
+	--	, lifeStage5
+	--	, [lifeStage_ind]
+	--  , SexMeas.sex
+	    , [sex] = CASE sexNoCount
+							WHEN 'male' THEN 'male'
+							WHEN 'female' THEN 'female'
+							WHEN 'female | male' THEN 'female | male'
+							WHEN 'male | female' THEN 'female | male'
+							ELSE NULL
+							END
+						
+	--	, [occurrenceRemarks] = COALESCE(SexMeas.sex, '') + 
+			--Case when COALESCE(SexMeas.sex, '') <> '' AND COALESCE(LifestageMeas.lifeStage, '') <> ''  THEN ' | ' ELSE '' END +
+			--COALESCE ( LifestageMeas.lifeStage5, '' ) 
+		, [occurrenceRemarks] = COALESCE(SexMeas.sex, '') + 
+			Case when COALESCE(SexMeas.sex, '') <> '' AND COALESCE(LifestageMeas.lifeStage, '') <> ''  THEN ' | ' ELSE NULL END +
+			COALESCE ( LifestageMeas.lifeStage5, '' ) 
+		
+		
+		
+		
+		
+		--, LifestageMeas.rauw as LifestagRauw
+		-- , SexMeas.rauw as SexRauw
+		
 
 -----Identification----
 
-		, [IdentifiedBy] = CASE WHEN LTRIM(RTRIM(COALESCE (RTRIM(LTRIM(I.[FORENAME])), RTRIM(LTRIM(I.[INITIALS])) ,'') + ' ' + COALESCE (RTRIM(LTRIM(I.[SURNAME])), ''))) = 'Unknown' THEN NULL
+		, [identifiedBy] = CASE WHEN LTRIM(RTRIM(COALESCE (RTRIM(LTRIM(I.[FORENAME])), RTRIM(LTRIM(I.[INITIALS])) ,'') + ' ' + COALESCE (RTRIM(LTRIM(I.[SURNAME])), ''))) = 'Unknown' THEN NULL
 							ELSE LTRIM(RTRIM(COALESCE (RTRIM(LTRIM(I.[FORENAME])), RTRIM(LTRIM(I.[INITIALS])) ,'') + ' ' + COALESCE (RTRIM(LTRIM(I.[SURNAME])), ''))) 
 							END
 
 ---- Taxonomic Elements --
 		
 		--[originalNameUsage] = T.ITEM_NAME,
-		, [ScientificName] = ns.RECOMMENDED_SCIENTIFIC_NAME     --recommended scientific soort + genus 
+		, [scientificName] = ns.RECOMMENDED_SCIENTIFIC_NAME     --recommended scientific soort + genus 
 		, [scientificNameAuthorship] = NS.RECOMMENDED_NAME_AUTHORITY + ISNULL ( ' ' + NS.RECOMMENDED_NAME_QUALIFIER , '') 
 		, [taxonRank] = lower(NS.RECOMMENDED_NAME_RANK_LONG)   
-		, [Kingdom]	  = CONVERT( nvarchar(50), N'Animalia' )
-	    , [Phylum]	  = CONVERT( nvarchar(50), N'Arthropoda' )
-	    , [Class]	  = CONVERT( nvarchar(50), N'Insecta' )
-	    , [Order]	  = CONVERT( nvarchar(50), N'Orthoptera' )
-		, [NomenclaturalCode] = 'ICZN'
+		, [kingdom]	  = CONVERT( nvarchar(50), N'Animalia' )
+	    , [phylum]	  = CONVERT( nvarchar(50), N'Arthropoda' )
+	    , [class]	  = CONVERT( nvarchar(50), N'Insecta' )
+	    , [order]	  = CONVERT( nvarchar(50), N'Orthoptera' )
+		, [nomenclaturalCode] = 'ICZN'
 		
 	
 -- Perqualifier
@@ -292,8 +331,8 @@ FROM dbo.Survey S
 											, [lifeStage_ind] = CASE WHEN MUMeas.SHORT_NAME = 'Count' AND MQMeas.SHORT_NAME in ('juviniel', 'juveniel') THEN 'juvenile'
 																	WHEN MUMeas.SHORT_NAME = 'Count' AND MQMeas.SHORT_NAME in ('Nimf') THEN 'nymph'
 																	WHEN MUMeas.SHORT_NAME = 'Count' AND MQMeas.SHORT_NAME IN ('Vrouw', 'Man', 'Adult Vrouw', 'Adult Man', 'zangpost') THEN 'adult' 
-																	WHEN MQMeas.SHORT_NAME IN ('none') THEN NULL
-																	ELSE MQMeas.SHORT_NAME  
+																	WHEN MQMeas.SHORT_NAME IN ('none','Vleugel') THEN NULL
+																	ELSE NULL  
 																	END
 											, [individualCount]  = sum( CASE WHEN ISNUMERIC(taoMeas.DATA) = 1 AND MUMeas.SHORT_NAME = 'Count' THEN CONVERT(int , taoMeas.DATA) ELSE NULL END )
 											, [individualNotCount]  = dbo.Concatenate(1 , CASE WHEN dbo.isReallyNumeric(taoMeas.DATA) = 0  THEN taoMeas.DATA ELSE NULL END , ',' )
@@ -311,8 +350,8 @@ FROM dbo.Survey S
 												, CASE WHEN MUMeas.SHORT_NAME = 'Count' AND MQMeas.SHORT_NAME in ('juviniel', 'juveniel') THEN 'juvenile'
 																	WHEN MUMeas.SHORT_NAME = 'Count' AND MQMeas.SHORT_NAME in ('Nimf') THEN 'nymph'
 																	WHEN MUMeas.SHORT_NAME = 'Count' AND MQMeas.SHORT_NAME IN ('Vrouw', 'Man', 'Adult Vrouw', 'Adult Man', 'zangpost') THEN 'adult' 
-																	WHEN MQMeas.SHORT_NAME IN ('none') THEN NULL
-																	ELSE MQMeas.SHORT_NAME  
+																	WHEN MQMeas.SHORT_NAME IN ('none','Vleugel') THEN NULL
+																	ELSE NULL  
 																	END
 												
 											) lifeStage
@@ -361,6 +400,7 @@ LEFT OUTER JOIN ( SELECT behaviour.TAXON_OCCURRENCE_KEY
 	--Sex Measurements
 	LEFT OUTER JOIN ( SELECT sex.TAXON_OCCURRENCE_KEY
 							, [sex] = dbo.Concatenate(1, sex.[sex_ind] + ':' + CONVERT(nvarchar(20), sex.[individualCount]), ' | ') 
+							, [sexNoCount] = dbo.Concatenate(1, sex.[sex_ind], ' | ')
 							, [individualCount] = sum(sex.[individualCount])
 							, [individualNotCount] = dbo.Concatenate(1, CASE WHEN LTRIM(RTRIM(sex.[individualNotCount]))  = '' THEN NULL ELSE sex.[individualNotCount] END  , ' | ' )
 							, [rauw] = dbo.Concatenate(1, sex.rauw , ' | ' ) 
@@ -405,6 +445,7 @@ AND TR.[SEQUENCE] >= 230
 AND LN.PREFERRED = 1
 AND TAO.CONFIDENTIAL = 0
 AND TAO.TAXON_OCCURRENCE_KEY NOT IN ('BFN0017900007ZKN','BFN0017900008DLI', 'BFN0017900008FNM','BFN0017900008RG5')
+
 
 
 
